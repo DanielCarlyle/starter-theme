b16d4afdfa0d90816580ce84e92f21a7dde4d8ea
Daniel Carlyle <dcarlyle@rto-ero.org>
2018-04-13 15:17:02 -0400

    added new gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const autoprefixer = require('gulp-autoprefixer');
 
 var SOURCEPATHS = {
   sassSource : 'src/scss/*.scss',
+  htmlSource : 'src/*html'
 }
 var APPPATH ={
   root: 'dist/',
@@ -13,11 +14,20 @@ var APPPATH ={
   js: 'dist/js'
 }
 
-gulp.task('sass', function() {
-   return gulp.src('SOURCEPATH.sassSource')
-   .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
-   .pipe(gulp.dest('APPPATH.css'));
+gulp.task('sass', function(){
+   return gulp.src(SOURCEPATHS.sassSource)
+   .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+    }))
+   .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+   .pipe(gulp.dest(APPPATH.css));
 });
+
+gulp.task('copy', function() {
+  gulp.src(SOURCEPATHS.htmlSource)
+      .pipe(gulp.dest(APPPATH.root))
+})
 
 gulp.task('serve', ['sass'], function() {
   browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
@@ -27,4 +37,9 @@ gulp.task('serve', ['sass'], function() {
   })
 });
 
-gulp.task('default', ['serve']);
+gulp.task('watch', ['serve', 'sass','copy'], function() {
+  gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+  gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
+});
+
+gulp.task('default', ['watch']);
